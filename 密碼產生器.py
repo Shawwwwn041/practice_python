4112a48f97d6e3a0f9108d8f032a08245ba61def
yamps1550 <yamps.1550@gmail.com>
2026-03-09 23:11:13 +0800

    測試

--- 密碼產生器.py
+++ 密碼產生器.py
@@ -7,14 +7,23 @@ eng = string.ascii_letters #建立英文字母的字串
 code_list = list(num+eng) #建立數字與字母的混合表
 random.shuffle(code_list) #利用random.shuffle語法將混合表打亂
 
-while True:
-    try:
-        code_len = int(input("你需要多長的密碼？"))#詢問使用者需要幾位數密碼
-        if code_len > 0:
-                code = "".join(code_list[:code_len])
-                print(f"已生成你所需的密碼：{code}")
-            else code_len <= 0:
-            print("輸入大於0的數字!")
+try:
+    code_len = int(input("你需要多長的密碼？"))#詢問使用者需要幾位數密碼
+    if code_len != 0:
+        code = "".join(code_list[:6])
+        print(f"已生成你所需的密碼：{code}")        
+    
+        
+except ValueError:
+    while True:
+        try:
+            code_len = int(input("你需要多長的密碼？")) #詢問使用者需要幾位數密碼
+            if code_len > 0:
+                break
+            else:
+                print("密碼長度不得為0！")
+        except ValueError:
+            print("請輸入有效的數字！")
 
-    except ValueError:
-        print("你輸入的格式錯誤!")
+code = "".join(code_list[:code_len])
+print(f"已生成你所需的密碼：{code}")
